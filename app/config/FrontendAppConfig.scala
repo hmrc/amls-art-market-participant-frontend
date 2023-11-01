3a8a049b4740db4767a8f7206ec90228397dc2be
jpatton638 <jpatton638@users.noreply.github.com>
2023-11-01 14:13:17 +0000

    DDCYLS-3718 | Alter construction of referrer URL for contact-frontend

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -26,20 +26,16 @@ import java.net.URLEncoder
 @Singleton
 class FrontendAppConfig @Inject() (configuration: Configuration) {
 
-  private def baseUrl(serviceName: String): String = {
-    val protocol = configuration.getOptional[String](s"microservice.services.$serviceName.protocol").getOrElse("http")
-    val host = configuration.get[String](s"microservice.services.$serviceName.host")
-    val port = configuration.get[String](s"microservice.services.$serviceName.port")
-    s"$protocol://$host:$port"
-  }
-
   val timeoutSeconds = configuration.get[String](s"timeout.seconds")
   val timeoutCountdown = configuration.get[String](s"timeout.countdown")
 
   def reportAProblemNonJSUrl(implicit request: Request[_]): String = {
     configuration.get[String]("microservice.services.contact-frontend.report-problem-url.non-js") +
     "&referrerUrl=" +
-    URLEncoder.encode(baseUrl("amls-art-market-participant-frontend") + request.uri, "utf-8")
+    URLEncoder.encode(
+      configuration.get[String](s"microservice.services.amls-art-market-participant-frontend.url") + request.uri,
+      "utf-8"
+    )
   }
   val betaFeedbackUrl = configuration.get[String]("microservice.services.contact-frontend.beta-feedback-url.authenticated")
   val betaFeedbackUnauthenticatedUrl = configuration.get[String]("microservice.services.contact-frontend.beta-feedback-url.unauthenticated")

2ebeac2b427350a74e3ed42f95653b204c42fd60
jpatton638 <jpatton638@users.noreply.github.com>
2024-04-09 16:33:05 +0100

    DDCYLS-4956 | Upgrade to Tutdor Crown

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "8.4.0"
+  private val bootstrapVersion = "8.5.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "8.4.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "8.5.0",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "2.0.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion
   )

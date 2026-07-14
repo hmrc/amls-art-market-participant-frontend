7c308ab1af6c034627f4a3c8b4dade0cb802c067
Vic Watson <171040641+VJWatson@users.noreply.github.com>
2026-07-14 11:00:54 +0100

    dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.31.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30"            % "12.32.1",
     "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.5.0",
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30"            % bootstrapVersion exclude("org.apache.commons", "commons-lang3"),
     "org.apache.commons"   % "commons-lang3"               % "3.18.0",
